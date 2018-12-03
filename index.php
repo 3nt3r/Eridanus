5c8d03a1991d5f2e31610d10dc3111438bb0655d
Jefferson <jeffersonbds_barros@hotmail.com>
2018-12-02 23:10:20 -0200

    Rodape modificado

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title></title>
+
+	<link rel="stylesheet" type="text/css" href="css/materialize.css">
+	<link rel="stylesheet" type="text/css" href="css/materialize.min.css">
+
+
+</head>
+<body>
+
+
+
+
+
+
+
+
+
+
+
+
+<?php 
+
+	include "rodape.php";
+
+?>
+
+
+</body>
+</html>
--- rodape.php
+++ rodape.php
@@ -1,4 +1,25 @@
-<?php 
-
-
-?>
+<footer class="page-footer light-green accent-4">
+  <div class="container">
+    <div class="row">
+      <div class="col l6 s12">
+        <img src="imagens/eridanus-pequeno.png">
+        <p class="grey-text text-lighten-4" style="font-size: 12px;">Mudanças são necessárias. Reciclagem não é só no meio ambiente, mas também no ambiente do nosso ser. - Daniel Carvalho</p>
+      </div>
+      <div class="col l4 offset-l2 s12">
+        <h5 class="white-text">Acompanhe-nos</h5>
+        <ul>
+          <li><a class="grey-text text-lighten-3" href="#!">Facebook</a></li>
+          <li><a class="grey-text text-lighten-3" href="#!">Instagram</a></li>
+          <li><a class="grey-text text-lighten-3" href="#!">Youtube</a></li>
+          <li><a class="grey-text text-lighten-3" href="#!">Fale Conosco</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="footer-copyright">
+    <div class="container">
+      © 2018 Copyright Calisto Softwares
+      <a class="grey-text text-lighten-4 right" href="https://softwarescalisto.000webhostapp.com">Desenvolvido por Calisto Softwares</a>
+    </div>
+  </div>
+</footer>
