3d0af5ffa1f19726369f150caa0a2b6b0b2fab1c
Ruben <rubenjosma11@gmail.com>
2018-12-04 17:38:57 -0300

    Editando menu

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 	<title></title>
 
-	<?php 
+	<?php
 
 		include "cabecalho.php";
 
@@ -13,11 +13,62 @@
 </head>
 <body>
 
-<?php 
+<?php
 
 	include "menu.php";
 
  ?>
+ <center><div id="paginas">
+
+ 	<div class="card" style="width: 18rem;">
+
+   <div class="card-body">
+     <center><h5 class="card-title">Projetos</h5></center>
+ 		<hr>
+ 		<p class="card-text">Nesta página, ficará disponível todos os projetos enviados pelos
+ 			usuários da plataforma Eridanus, logo após da equipe Eridanus realizar a aprovação do
+ 			mesmo</p>
+     <!--<a href="#" class="btn btn-primary">Go somewhere</a>-->
+   </div>
+ 	<center><img class="img-card" src="imagens/preto.jpg" width="250" height="60" alt="Card image cap"></center>
+ </div>
+ <div class="card" style="width: 18rem;">
+
+ <div class="card-body">
+ 	<center><h5 class="card-title">Resídios</h5></center>
+ 	<hr>
+ 	<p class="card-text">Veja informações sobre os resíduos eletrônicos
+ no brasil, bem como, informações sobre a decomposição dos resíduos e quais são úteis
+ para serem reciclados ou não.</p>
+ 	<!--<a href="#" class="btn btn-primary">Go somewhere</a>-->
+ </div>
+ <center><img class="img-card" src="imagens/preto.jpg" width="250" height="60" alt="Card image cap"></center>
+ </div>
+ <div class="card" style="width: 18rem;">
+
+ <div class="card-body">
+ 	<center><h5 class="card-title">Locais</h5></center>
+ 	<hr>
+ 	<p class="card-text">Nesta página, estará disponível locais na cidade de Salgueiro - PE, onde os
+ 	usuários poderão descartar os resíduos e também, locais que apoiam o projeto de
+ 	alguma forma.</p>
+ 	<!--<a href="#" class="btn btn-primary">Go somewhere</a>-->
+ </div>
+ <center><img class="img-card" src="imagens/preto.jpg" width="250" height="60" alt="Card image cap"></center>
+ </div>
+
+ <div class="card" style="width: 18rem;">
+
+ <div class="card-body">
+ 	<center><h5 class="card-title">Trocas</h5></center>
+ 	<hr>
+ 	<p class="card-text"> Nesta tela, ficará a amostra, todos os anúncios de resíduos dos usuários. Ao
+ clicar no botão, você será redirecionado a bate-papo diretamente com o anunciante.</p>
+ 	<!--<a href="#" class="btn btn-primary">Go somewhere</a>-->
+ </div>
+ <center><img class="img-card" src="imagens/preto.jpg" width="250" height="60" alt="Card image cap"></center>
+ </div>
+ </div></center>
 
 
 
