666aeac4821419fa87b87bb0e66e4ce3eaa12743
Ruben <rubenjosma11@gmail.com>
2018-12-04 12:01:54 -0300

    Rodape colocado no final da pagina

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <head>
 
 	<title></title>
-
 	<link rel="stylesheet" type="text/css" href="css/materialize.css">
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
 	<link rel="stylesheet" type="text/css" href="css/materialize.min.css">
 
 
@@ -18,11 +18,7 @@
 
 
 
-
-
-
-
-<?php 
+<?php
 
 	include "rodape.php";
 
