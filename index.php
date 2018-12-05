e8103be69812e2d413776c9d7d992f4272019cc0
Ruben <rubenjosma11@gmail.com>
2018-12-05 18:40:26 -0300

    Adicionando cartões

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 	<title></title>
 
-	<?php 
+	<?php
 
 		include "cabecalho.php";
 
@@ -13,13 +13,70 @@
 </head>
 <body>
 
-<?php 
+<?php
 
 	include "menu.php";
 
  ?>
 
 
+  <div class="row">
+     <div class="col s12 m6 l3">
+       <div class="card write">
+         <div class="card-content white-text">
+           <span class="card-title black-text">Projetos</span>
+					 <hr>
+					 <p class="black-text">Nesta página, ficará disponível todos os projetos enviados pelos
+ 				    usuários da plataforma Eridanus, logo após da equipe Eridanus realizar a aprovação do
+ 				    mesmo.</p>
+         </div>
+         <div class="card-action">
+           <center><a href="projetos-usuarios.php">Saiba mais</a></center>
+         </div>
+       </div>
+     </div>
+ 		<div class="col s12 m6 l3">
+       <div class="card write">
+         <div class="card-content white-text">
+           <span class="card-title black-text">Resíduos</span>
+					 <hr>
+           <p class="black-text">Veja informações sobre os resíduos eletrônicos
+ 					no brasil, bem como, informações sobre a decomposição dos resíduos e quais são úteis
+ 					para serem reciclados ou não.</p>
+         </div>
+         <div class="card-action">
+           <center><a href="residuos.php">Saiba mais</a></center>
+         </div>
+       </div>
+     </div>
+ 		<div class="col s12 m6 l3">
+       <div class="card write">
+         <div class="card-content white-text">
+           <span class="card-title black-text">Locais</span>
+					 <hr>
+           <p class="black-text">Nesta página, estará disponível locais na cidade de Salgueiro - PE, onde os
+ 					usuários poderão descartar os resíduos e também, locais que apoiam o projeto de
+ 					alguma forma.</p>
+         </div>
+         <div class="card-action">
+           <center><a href="locais.php">Saiba mais</a></center>
+         </div>
+       </div>
+     </div>
+ 		<div class="col s12 m6 l3">
+       <div class="card write">
+         <div class="card-content white-text">
+           <span class="card-title black-text">Trocas</span>
+					 <hr>
+           <p class="black-text">Nesta tela, ficará a amostra, todos os anúncios de resíduos dos usuários. Ao
+ 					clicar no botão, você será redirecionado a bate-papo diretamente com o anunciante.</p>
+         </div>
+         <div class="card-action">
+           <center><a href="trocas.php">Saiba mais</a></center>
+         </div>
+       </div>
+     </div>
+   </div>
 
 <br>
 <br>
