d9c5f2136a9bbc6cbf42277369ec53a5de9a4f72
Ruben <rubenjosma11@gmail.com>
2018-12-07 23:22:55 -0300

    Carrossel adicionado

--- cabecalho.php
+++ cabecalho.php
@@ -1,7 +1,4 @@
-<link rel="shortcut icon" href="imagens/icone-eridanus.png"> 
+<link rel="shortcut icon" href="imagens/icone-eridanus.png">
 <link rel="stylesheet" type="text/css" href="css/estilo.css">
 <link rel="stylesheet" type="text/css" href="css/materialize.min.css">
 <link rel="stylesheet" type="text/css" href="css/materialize.css">
-<script type="text/javascript" src="js/materialize.js"></script>
-<script type="text/javascript" src="js/materialize.min.js"></script>
-<script type="text/javascript" src="js/script.js"></script>
--- index.php
+++ index.php
@@ -18,8 +18,25 @@
 	include "menu.php";
 
  ?>
-
-
+ <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+ <script type="text/javascript" src="js/materialize.js"></script>
+ <script type="text/javascript" src="js/materialize.min.js"></script>
+ <script type="text/javascript" src="js/script.js"></script>
+  <div class="carousel carousel-slider center">
+    <div class="carousel-item red white-text" href="#one!" style="background-image: url('imagens/lixo-eletro.jpg'); background-repeat: no-repeat; background-size: 100% 100%;">
+      <p class="white-text" style="font-size: 1.4em; margin-top: 22%; text-shadow: 0.1em 0.1em #333;">Lixo eletrônico é tudo aquilo e você vê, lê e acha que não serve para nada, então outros reciclam, selecionam e constroi o seu pensamento.</p>
+    </div>
+    <div class="carousel-item amber white-text" href="#two!" style="background-image: url('imagens/placas.jpg'); background-repeat: no-repeat; background-size: 100% 100%;">
+      <p class="white-text" style="font-size: 1.4em; margin-top: 22%; text-shadow: 0.1em 0.1em #333;">Se o descarte do lixo eletrônico for feito de modo incorreto, pode liberar resíduos tóxicos que contaminam o solo e os lençóis freáticos, colocando em risco a saúde pública.</p>
+    </div>
+  </div>
+<script type="text/javascript">$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+</script>
+<br>
+<br>
   <div class="row">
      <div class="col s12 m6 l3">
        <div class="card write">
@@ -81,9 +98,6 @@
 <br>
 <br>
 <br>
-<br>
-<br>
-
 
 <?php
 
