c6b98a370c675b4dd045525c198caf0495a3ef94
Jefferson <jeffersonbds_barros@hotmail.com>
2018-12-16 16:17:48 -0200

    Pagina cadastre-se funcionando (sem qualquer tipo de validacao ainda)

--- cadastra.php
+++ cadastra.php
@@ -20,6 +20,63 @@
  ?>
 
 
+<?php 
+/////////////////////////////////////////////////////////////////////////////
+
+	$nome = htmlspecialchars($_POST['nome_cad']);
+	$sobrenome = htmlspecialchars($_POST['sobrenome_cad']);
+	$email = htmlspecialchars($_POST['email_cad']);
+	$senha = htmlspecialchars($_POST['senha_cad']);
+	$senhacomp = htmlspecialchars($_POST['senhacomp_cad']);
+
+	if(!empty($nome) && !empty($sobrenome) && !empty($email) && !empty($senha) && !empty($senhacomp)){
+		if($senha == $senhacomp){
+
+			///////////////////////
+			$banco = new mysqli('localhost', 'usuario_banco', 'senha_banco', 'nome_banco');
+			///////////////////////
+			if(mysqli_connect_errno()){
+				?>
+					<h3 class="titulo-pagina">Não foi possível a conexão com o banco de dados. Por favor, tente mais tarde!</h3>
+				<?php
+				exit;
+			}
+
+			$nome = strval($nome);
+			$sobrenome = strval($sobrenome);
+			$email = strval($email);
+			$senha = strval($senha);
+
+
+
+
+
+			$inserir = "INSERT INTO usuario (nome, sobrenome, email, senha) VALUES (?, ?, ?, ?)";
+
+			$prepare = $banco->prepare($inserir);
+			$prepare->bind_param("ssss", $nome, $sobrenome, $email, $senha);
+			$prepare->execute();
+
+
+
+			//////////////////////
+
+		}else{
+			?>
+				<h3 class="titulo-pagina">As senhas não batem, tente novamente!</h3>
+			<?php
+			exit;
+		}
+	}else{
+		?>
+			<h3 class="titulo-pagina">Por favor, preencha todos os dados!</h3>
+		<?php
+		exit;
+	}
+
+
+/////////////////////////////////////////////////////////////////////////////
+?>
 
 
 
