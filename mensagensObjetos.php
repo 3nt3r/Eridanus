fdecf222d12a4d0dca65a39f36336d5dbb719991
Ruben <rubenjosma11@gmail.com>
2019-04-20 19:10:27 -0300

    Correção de Bug.

--- mensagensObjetos.php
+++ mensagensObjetos.php
@@ -8,10 +8,10 @@
 
   include "conexao.php";
 
-  $consulta = "SELECT mensagem.conteudo, mensagem.id_emi, mensagem.id_rec, mensagem.id_objeto, objeto.nome FROM mensagem, objeto WHERE id_rec = ? GROUP BY conteudo";
+  $consulta = "SELECT mensagem.conteudo, mensagem.id_emi, mensagem.id_rec, mensagem.id_objeto, objeto.nome, usuario.nome FROM mensagem INNER JOIN objeto on mensagem.id_rec = ? inner join usuario on mensagem.id_emi = usuario.id";
   $prepare = $banco->prepare($consulta);
   $prepare->bind_param("i", $_SESSION['id']);
-  $prepare->bind_result($mensagem, $remetente, $destinatario, $idObjeto, $nomeObjeto);
+  $prepare->bind_result($mensagem, $remetente, $destinatario, $idObjeto, $nomeObjeto, $nomeRemetente);
   $prepare->execute();
   $prepare->store_result();
 
@@ -40,9 +40,9 @@
     echo "
       <tr>
 
-        <td> $remetente </td>
+        <td> $nomeRemetente </td>
 
-        <td> 
+        <td>
 
           <a class='light-green accent-4 btn modal-trigger' href='#modal$contadorModal'> Ver </a>
 
