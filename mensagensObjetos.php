810880c358a2805509dd78349b2040bb39405134
Jefferson <jeffersonbds_barros@hotmail.com>
2019-04-22 21:46:25 -0300

    Correção bug mensagem

--- mensagensObjetos.php
+++ mensagensObjetos.php
@@ -8,7 +8,7 @@
 
   include "conexao.php";
 
-  $consulta = "SELECT mensagem.conteudo, mensagem.id_emi, mensagem.id_rec, mensagem.id_objeto, objeto.nome, usuario.nome FROM mensagem INNER JOIN objeto on mensagem.id_rec = ? inner join usuario on mensagem.id_emi = usuario.id";
+  $consulta = "SELECT mensagem.conteudo, mensagem.id_emi, mensagem.id_rec, mensagem.id_objeto, objeto.nome, usuario.nome FROM mensagem INNER JOIN objeto on mensagem.id_rec = ? inner join usuario on mensagem.id_emi = usuario.id GROUP BY mensagem.conteudo";
   $prepare = $banco->prepare($consulta);
   $prepare->bind_param("i", $_SESSION['id']);
   $prepare->bind_result($mensagem, $remetente, $destinatario, $idObjeto, $nomeObjeto, $nomeRemetente);
